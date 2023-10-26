d48cc1019da1e780cfe8f56e1ba127104341ca98
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-10-26 19:42:55 +0100

    Update TemplatePluginMV.js

--- TemplatePluginMV.js
+++ TemplatePluginMV.js
@@ -1,1 +1,21 @@
+//=============================================================================
+// TemplatePluginMV.js
+//=============================================================================
+/*:
+ * @plugindesc |Add a description here!|
+ *
+ * @author |Add your name here!|
+ *
+ * @help |Add additional info here!|
+ *
+ * TERMS OF USE
+ * Add license information and permissions here!
+ *
+ * COMPATIBILITY
+ * Note any changes that could cause issues here!
+ */
 
+function TemplateFunction() 
+{
+  //Some code goes here!
+}
